5f4522fac8e3ebccc0b68cb2c016713009755d6d
Aisha Smith <aisha.smith413@gmail.com>
2023-04-22 14:45:49 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './images/eye.png';
 import './App.css';
 
 function App() {
@@ -6,16 +6,26 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        <h1>
+        <code>Hi, my name is Aisha 👋🏾</code>
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <code>Aisha 💗's to code!🖥️</code>
+        </p>
+        <p>
+        <code> She is also a talented Portait Artist!👩‍🎨</code>
+        </p>
+        <p>
+        <code>And...loves to bake yummy Cakes!😍🧁</code>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/aisha-s-7b930ab6/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Contact Me
         </a>
       </header>
     </div>
